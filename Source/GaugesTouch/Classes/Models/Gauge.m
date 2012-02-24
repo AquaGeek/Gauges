e414c1ad80a39ae933bad231ab2243852f8f6065
Tyler Stromberg <tylers@mac.com>
2012-02-23 22:37:17 -0500

    Add last week traffic property; update temp data generator to use rand()

--- Source/GaugesTouch/Classes/Models/Gauge.m
+++ Source/GaugesTouch/Classes/Models/Gauge.m
@@ -48,25 +48,41 @@
 
 #pragma mark - Traffic
 
+- (NSArray *)weekTraffic
+{
+    NSArray *traffic = self.recentTraffic;
+    NSRange weekRange;
+    
+    if (traffic.count > 7)
+    {
+        weekRange.location = traffic.count - 7;
+    }
+    
+    weekRange.length = MIN(7, traffic.count);
+    
+    return [traffic subarrayWithRange:weekRange];
+}
+
 - (void)refreshTrafficWithHandler:(void (^)(NSError *error))completionHandler
 {
+    srand(time(NULL));
     double delayInSeconds = 2.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         //!!! TEMP: Fake some traffic
         NSDictionary *todaysTrafficDict = [NSDictionary dictionaryWithObjectsAndKeys:@"2012-02-23", @"date",
-                                           [NSNumber numberWithUnsignedLongLong:654321], @"views",
-                                           [NSNumber numberWithUnsignedLongLong:123456], @"people", nil];
+                                           [NSNumber numberWithLong:rand() % 1000 + 1000], @"views",
+                                           [NSNumber numberWithLong:rand() % 1000], @"people", nil];
         self.todayTraffic = [[DatedViewSummary alloc] initWithDictionary:todaysTrafficDict];
         
         NSMutableArray *newTraffic = [NSMutableArray arrayWithCapacity:30];
         
-        for (NSInteger i = 1; i < 24; i++)
+        for (NSInteger i = 1; i < 23; i++)
         {
             NSString *dateString = [NSString stringWithFormat:@"2012-02-%02d", i];
             NSDictionary *trafficDict = [NSDictionary dictionaryWithObjectsAndKeys:dateString, @"date",
-                                         [NSNumber numberWithInt:1234], @"views",
-                                         [NSNumber numberWithInt:321], @"people", nil];
+                                         [NSNumber numberWithLong:rand() % 1000 + 1000], @"views",
+                                         [NSNumber numberWithLong:rand() % 1000], @"people", nil];
             
             DatedViewSummary *traffic = [[DatedViewSummary alloc] initWithDictionary:trafficDict];
             [newTraffic addObject:traffic];
