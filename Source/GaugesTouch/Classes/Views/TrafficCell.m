b46541fde1a586f7780cc4cb4b9eabbbe43aea40
Tyler Stromberg <tylers@mac.com>
2012-03-07 21:01:08 -0500

    Add TrafficCell class

---
+++ Source/GaugesTouch/Classes/Views/TrafficCell.m
@@ -0,0 +1,34 @@
+//
+//  TrafficCell.m
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/7/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import "TrafficCell.h"
+
+@implementation TrafficCell
+
+@synthesize dateLabel = _dateLabel;
+@synthesize viewsLabel = _viewsLabel;
+@synthesize peopleLabel = _peopleLabel;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]))
+    {
+        // Initialization code
+    }
+    
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
