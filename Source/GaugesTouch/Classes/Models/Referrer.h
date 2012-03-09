5a2c1938435081957c0c7c70c370dba9c4f12196
Tyler Stromberg <tylers@mac.com>
2012-03-09 01:32:33 -0500

    Add referrer model

---
+++ Source/GaugesTouch/Classes/Models/Referrer.h
@@ -0,0 +1,20 @@
+//
+//  Referrer.h
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/9/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Referrer : NSObject
+
+@property (nonatomic, strong) NSString *host;
+@property (nonatomic) long views;
+@property (nonatomic, strong) NSString *path;
+@property (nonatomic, strong) NSString *url;
+
+- (NSString *)formattedViews;
+
+@end
