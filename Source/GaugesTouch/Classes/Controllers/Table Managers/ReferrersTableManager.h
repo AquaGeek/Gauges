7a5a0d7027815b9b7f50c71840b6ecb52ec7c4a5
Tyler Stromberg <tylers@mac.com>
2012-03-09 01:29:43 -0500

    Create referrers table manager

---
+++ Source/GaugesTouch/Classes/Controllers/Table Managers/ReferrersTableManager.h
@@ -0,0 +1,13 @@
+//
+//  ReferrersTableManager.h
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/9/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import "TableManager.h"
+
+@interface ReferrersTableManager : TableManager
+
+@end
---
+++ Source/GaugesTouch/Classes/Controllers/Table Managers/ReferrersTableManager.m
@@ -0,0 +1,20 @@
+//
+//  ReferrersTableManager.m
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/9/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import "ReferrersTableManager.h"
+
+@implementation ReferrersTableManager
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    self.title = NSLocalizedString(@"Top Referrers", nil);
+}
+
+@end
