3e3f3c9bbb1be16c4b7caf0d620d02aed0b33cd6
Tyler Stromberg <tylers@mac.com>
2012-03-09 21:35:16 -0500

    Create web view storyboard scene. Add toolbar and button images

---
+++ Source/GaugesTouch/Classes/Controllers/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/9/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController <UIWebViewDelegate>
+
+@end
---
+++ Source/GaugesTouch/Classes/Controllers/WebViewController.m
@@ -0,0 +1,47 @@
+//
+//  WebViewController.m
+//  GaugesTouch
+//
+//  Created by Tyler Stromberg on 3/9/12.
+//  Copyright (c) 2012 Tyler Stromberg. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController()
+
+- (IBAction)reloadTapped:(id)sender;
+- (IBAction)backTapped:(id)sender;
+- (IBAction)forwardTapped:(id)sender;
+- (IBAction)openInSafariTapped:(id)sender;
+
+@end
+
+
+#pragma mark -
+
+@implementation WebViewController
+
+#pragma mark Button Actions
+
+- (IBAction)reloadTapped:(id)sender
+{
+    // TODO: Reload the web view and swap out the reload button for a stop button
+}
+
+- (IBAction)backTapped:(id)sender
+{
+    // TODO: Go back
+}
+
+- (IBAction)forwardTapped:(id)sender
+{
+    // TODO: Go forward
+}
+
+- (IBAction)openInSafariTapped:(id)sender
+{
+    // TODO: Open the URL in Mobile Safari
+}
+
+@end
