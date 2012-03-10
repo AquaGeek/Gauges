aa2e2677bcaa315a72e499a7d3a34985ab55b55c
Tyler Stromberg <tylers@mac.com>
2012-03-09 21:49:00 -0500

    Wire up the buttons and web view delegate methods

--- Source/GaugesTouch/Classes/Controllers/WebViewController.m
+++ Source/GaugesTouch/Classes/Controllers/WebViewController.m
@@ -10,9 +10,13 @@
 
 @interface WebViewController()
 
-- (IBAction)reloadTapped:(id)sender;
-- (IBAction)backTapped:(id)sender;
-- (IBAction)forwardTapped:(id)sender;
+@property (nonatomic, weak) IBOutlet UIWebView *webView;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *reloadItem;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *stopItem;
+@property (nonatomic, weak) IBOutlet UIButton *backButton;
+@property (nonatomic, weak) IBOutlet UIButton *forwardButton;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *activityIndicator;
+
 - (IBAction)openInSafariTapped:(id)sender;
 
 @end
@@ -22,26 +26,57 @@
 
 @implementation WebViewController
 
+@synthesize webView = _webView;
+@synthesize reloadItem = _reloadItem;
+@synthesize stopItem = _stopItem;
+@synthesize backButton = _backButton;
+@synthesize forwardButton = _forwardButton;
+@synthesize activityIndicator = _activityIndicator;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Remove the stop item from the nav bar - we just set it up that way because it's easiest
+    self.navigationItem.leftBarButtonItem = nil;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
+}
+
 #pragma mark Button Actions
-
-- (IBAction)reloadTapped:(id)sender
-{
-    // TODO: Reload the web view and swap out the reload button for a stop button
-}
-
-- (IBAction)backTapped:(id)sender
-{
-    // TODO: Go back
-}
-
-- (IBAction)forwardTapped:(id)sender
-{
-    // TODO: Go forward
-}
 
 - (IBAction)openInSafariTapped:(id)sender
 {
     // TODO: Open the URL in Mobile Safari
+}
+
+
+#pragma mark - UIWebView Delegate Methods
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    // Swap out the reload button for the stop button
+    self.navigationItem.rightBarButtonItem = self.stopItem;
+    
+    [self.activityIndicator startAnimating];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    // Swap out the stop button for the reload button
+    self.navigationItem.rightBarButtonItem = self.reloadItem;
+    
+    [self.activityIndicator stopAnimating];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    // TODO: Anything?
 }
 
 @end
